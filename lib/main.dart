2d799c586487894c3c59b106a7b66740bdbbc4ab
jrrodrigo421 <rjr89000@gmail.com>
2023-11-14 21:26:08 -0300

    Row

--- lib/main.dart
+++ lib/main.dart
@@ -11,22 +11,20 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: Column(
-        // alignment: AlignmentDirectional.center,
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: [
-          Container(height: 300, width: 300, color: Colors.black),
-          Container(height: 150, width: 150, color: Colors.red),
-          Container(height: 75, width: 75, color: Colors.blue),
-          Container(height: 37, width: 37, color: Colors.yellow),
-        ],
-      ),
-    );
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          // crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            // Container(height: 300, width: 300, color: Colors.black),
+            Container(height: 150, width: 150, color: Colors.red),
+            Container(height: 75, width: 75, color: Colors.blue),
+            Container(height: 37, width: 37, color: Colors.yellow),
+          ],
+        ));
   }
 }
