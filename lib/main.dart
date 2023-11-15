cb52d7403d096967f0a99907c77af7fa04dd8443
jrrodrigo421 <rjr89000@gmail.com>
2023-11-15 18:45:42 -0300

    criando a listagem de task e parametro para diferenciar as task

--- lib/main.dart
+++ lib/main.dart
@@ -19,12 +19,60 @@ class MyApp extends StatelessWidget {
           backgroundColor: Colors.deepOrange,
           title: const Text('Lista de Tarefas'),
         ),
-        body: Container(
-          color: Colors.blueGrey,
+        body: const Column(
+          children: [
+            Task(nome: 'Dominar Dart',),
+            Task(nome: 'Dominar Flutter'),
+            Task(nome: 'Dominar leitura de documentação',),
+            Task(nome: 'Dominar UI/UX'),
+            Task(nome: 'Dominar TUDO! >> #FÉ'),
+
+          ],
         ),
-        floatingActionButton: FloatingActionButton(onPressed: () {
-          print('clicou');
-        }),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            print('clicou');
+          },
+          backgroundColor: Colors.deepOrangeAccent,
+        ),
+      ),
+    );
+  }
+}
+
+class Task extends StatelessWidget {
+  final String nome;
+
+  const Task({super.key, required this.nome});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.blueGrey,
+      child: Stack(
+        children: [
+          Container(
+            color: Colors.deepOrangeAccent,
+            height: 140,
+          ),
+          Container(
+            color: const Color.fromARGB(255, 192, 200, 199),
+            height: 100,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Container(
+                  color: Colors.blueGrey,
+                  width: 72,
+                  height: 100,
+                ),
+                Text(nome),
+                ElevatedButton(
+                    onPressed: () {}, child: Icon(Icons.arrow_drop_up))
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
